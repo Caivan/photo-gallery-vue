cbe9ccee0a23e5c69baddad0da2f655af2477465
Caivan <caivan2@gmail.com>
2020-10-02 20:48:07 -0500

    Restructure navigation methods

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -2,7 +2,9 @@
   <div>
     <div class="navigation item">
       <div>
-        <label for="pageSizeField" class="noselect">Set the size of each page</label>
+        <label for="pageSizeField" class="noselect"
+          >Set the size of each page</label
+        >
         <input
           name="pageSizeField"
           v-model="pageSize"
@@ -11,45 +13,56 @@
           max="100"
           min="1"
         />
-        <br>
+        <br />
         <label for="filter" class="noselect">Filter by author</label>
         <input
           name="filter"
           v-model="filterAuthor"
           placeholder="author"
-          type="text"          
+          type="text"
         />
       </div>
       <div id="pagination">
         <button
           class="noselect"
-          v-if="currentPage > 1"
-          v-on:click="loadPage('previous')"
+          v-if="!isFirstPage"
+          @click="loadPage('previous')"
         >
           Previous
         </button>
 
         <input
           v-model="inputPageNumber"
-          placeholder="Select Page and Press Enter"
-          type="number"          
+          placeholder="Select Page"
+          type="number"
           min="1"
         />
-        <button class="noselect" v-on:click="loadSpecificPage()">Go</button>
-        <button class="noselect" v-on:click="loadPage('next')">Next</button>
+        <button           
+          class="noselect" 
+          @click="loadPage('specific')">
+          Go
+        </button>
+        <button
+          v-if="!isLastPage"
+          class="noselect"
+          @click="loadPage('next')"
+        >
+          Next
+        </button>
       </div>
     </div>
 
-
     <div class="imagecontainer">
+      <div v-if="messageUser">
+          {{ messageUser }}
+      </div>
       <image-element
         v-for="image in filterImagesByAuthor"
         v-bind:key="image.id"
-        class="item"        
+        class="item"
         v-bind:imageElement="image"
       ></image-element>
     </div>
-
   </div>
 </template>
 
@@ -70,64 +83,56 @@ export default {
       inputPageNumber: 1,
       pageSize: 100,
       filterAuthor: '',
+      isLastPage: false,
+      isFirstPage: true,
+      messageUser: ''
     };
   }, // End Data
   computed : {
-    filterImagesByAuthor() {      
-      return this.imagesList.filter(a => a.author.toLowerCase().includes(this.filterAuthor.toLowerCase()));        
-    }// End filtering function    
+    filterImagesByAuthor() {
+      return this.imagesList.filter(a => a.author.toLowerCase().includes(this.filterAuthor.toLowerCase()));
+    }// End filtering function
   },
   created() {
-    picusmApi.getAllImages().then((imagesResponse) => this.imagesList = imagesResponse);
+    this.makeDataRequest ();
   }, // End Created
   watch: {
     pageSize: _.debounce(function () {
-        this.pageSize = this.pageSize > 100 || this.pageSize < 1 ? 100 : this.pageSize;
-        picusmApi.getAllImages(this.inputPageNumber, this.pageSize)
-          .then((imagesResponse) => {
-            if (imagesResponse && imagesResponse.length > 0) {
-              this.imagesList = imagesResponse;
-            } else {
-              this.inputPageNumber = this.currentPage;
-            }
-          });
+        this.pageSize = this.pageSize > 100 || this.pageSize < 0 ? 100 : this.pageSize;
+        this.makeDataRequest();
     }, 500)
   },
   methods: {
+    makeDataRequest (specificPage=this.currentPage){
+      picusmApi.getAllImages(specificPage, this.pageSize ? this.pageSize : 100 ).then((response) => {            
+            response.results.then (results => {
+                if (results && results.length > 0){                  
+                  this.isLastPage = response.isLastPage,
+                  this.isFirstPage = response.isFirstPage,
+                  this.imagesList=results;
+                  this.messageUser = ''
+                } else {
+                  this.messageUser = 'Any data returned for the requested page';
+                }
+            });            
+      });
+    },
     loadPage: _.debounce(function (stepDirection) {
       if (this.currentPage >= 1) {
-        stepDirection === "next"
-          ? (this.currentPage += 1)
-          : (this.currentPage -= 1);
-        picusmApi
-          .getAllImages(this.currentPage, this.pageSize)
-          .then((imagesResponse) => {
-            if (imagesResponse && imagesResponse.length > 0) {
-              this.imagesList = imagesResponse;
-              this.inputPageNumber = this.currentPage;
-            } else {
-              stepDirection === "next"
-                ? (this.currentPage -= 1)
-                : (this.currentPage += 1);
-            }
-          });
-      }
+        if (stepDirection === "next" && !this.isLastPage) {
+          this.currentPage += 1;
+          this.makeDataRequest ();
+        }else if (stepDirection === "previous" && !this.isFirstPage) {
+          this.currentPage -= 1;
+          this.makeDataRequest ();
+        }else if (stepDirection === "specific") {          
+          if (this.currentPage != parseInt(this.inputPageNumber)) {              
+              this.makeDataRequest (this.inputPageNumber);
+          }
+        }        
+      } 
+      this.inputPageNumber = this.currentPage;
     }, 500), // End function
-    loadSpecificPage: _.debounce(function () {
-      if (this.currentPage != parseInt(this.inputPageNumber)) {
-        let pageNumber = parseInt(this.inputPageNumber);
-        picusmApi
-          .getAllImages(pageNumber, this.pageSize)
-          .then((imagesResponse) => {
-            if (imagesResponse && imagesResponse.length > 0) {
-              this.imagesList = imagesResponse;
-              this.currentPage = pageNumber;
-            } else {
-              this.inputPageNumber = this.currentPage;
-            }
-          });
-      }
-    }, 100), // End function
   }, // End Methods
 };
 </script>
@@ -148,7 +153,7 @@ button {
   grid-template-columns: repeat(auto-fill, 250px);
   grid-gap: 15px;
   justify-content: center;
-  margin: 20px;  
+  margin: 20px;
 }
 .item:nth-of-type(1) {
   grid-column: 1;
@@ -158,19 +163,17 @@ button {
   grid-template-rows: 50% 50%;
   grid-gap: 15px;
   justify-content: center;
-  margin: 20px;  
+  margin: 20px;
 }
 
 @media screen and (max-width: 760px) {
   .imagecontainer {
-      grid-template-columns: 50% 50%;
+    grid-template-columns: 50% 50%;
   }
-  
 }
 @media screen and (max-width: 412px) {
   .imagecontainer {
-      grid-template-columns: 100%;
+    grid-template-columns: 100%;
   }
-  
 }
 </style>
